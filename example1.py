75b8da6584ed653c31d5686906a5c0374d0f8982
Sarthak2601 <agarwal.sarthak262012@gmail.com>
2020-04-06 01:48:40 +0530

    part3

--- example1.py
+++ example1.py
@@ -1,20 +1,33 @@
 import pandas as pd
 import quandl, math
 import numpy as np
-from sklearn import preprocessing
+from sklearn import preprocessing, svm
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
-df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Low'] * 100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
 
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
-
 forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
-forecast_out = (math.ceil(0.01*len(df)))
-# print(math.ceil(0.01*len(df)))
+forecast_out = (math.ceil(0.01 * len(df)))
+print(forecast_out)
 df['lable'] = df[forecast_col].shift(-forecast_out)
 df.dropna(inplace=True)
-print(df.head())
+
+X = np.array(df.drop('lable', 1))
+y = np.array(df['lable'])
+X = preprocessing.scale(X)
+y = np.array(df['lable'])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+clf = LinearRegression()
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+
+print(f"{accuracy * 100}%")
